8714734b2ab9225704e5862fcc3731c9c4a8df1e
deepukumar4626 <deepu.kumar@avidialabs.com>
2025-01-23 00:46:48 +0530

    5.0

--- demoapp/settings.py
+++ demoapp/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -75,12 +77,15 @@ WSGI_APPLICATION = 'demoapp.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'railway',
+        'USER': 'root',
+        'HOST': 'mysql-p59x.railway.internal',
+        'PORT': '3306',
+        'PASSWORD': 'ttxDryxOPnViuYikQmYdthyiwbdOPutA',
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
